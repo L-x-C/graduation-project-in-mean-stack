8f4202bb7792f41b8ce097acc4d285d76f6fe524
L_x_C <lxc.neal@gmail.com>
2015-02-17 15:29:57 +0800

    ui-route

--- public/build/js/all.js
+++ public/build/js/all.js
@@ -3,10 +3,19 @@
  *
  * Description
  */
-var myApp = angular.module('myApp', ['ngMaterial','ngAnimate']);
-myApp.config(function($mdThemingProvider) {
-	// Configure a dark theme with primary foreground yellow
+angular.module('myApp', ['ngMaterial','ui.router'])
+.config(function($mdThemingProvider, $stateProvider, $urlRouterProvider) {
+	// configure a dark theme
 	$mdThemingProvider.theme('docs-dark', 'default')
 		.primaryPalette('grey')
 		.dark();
+
+
+	$urlRouterProvider.when("","/");
+	$stateProvider
+		.state("/", {
+			url: "/",
+			templateUrl: "../views/partials/main.html",
+			controller: 'indexCtrl'
+		});
 });
