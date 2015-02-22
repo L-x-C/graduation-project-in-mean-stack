ee2f663b5a7dab290ac8ee746e0328104a8e47f5
L_x_C <lxc.neal@gmail.com>
2015-02-22 15:40:40 +0800

    rm

--- public/javascripts/controllers/detail.js
+++ public/javascripts/controllers/detail.js
@@ -1,13 +1,4 @@
 angular.module('myApp')
 .controller('detailCtrl', function($scope,$mdBottomSheet) {
-	$scope.showGridBottomSheet = function($event) {
-		$scope.alert = '';
-		$mdBottomSheet.show({
-			templateUrl: '/views/partials/bottom-sheet-grid-template.html',
-			controller: 'GridBottomSheetCtrl',
-			targetEvent: $event
-		}).then(function(clickedItem) {
-			$scope.alert = clickedItem.name + ' clicked!';
-		});
-	};
+
 })
