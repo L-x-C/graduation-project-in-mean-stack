58bb6a1b4975f753dac7e85908f7f8b487310250
L_x_C <lxc.neal@gmail.com>
2015-02-22 16:06:01 +0800

    temp

--- public/javascripts/controllers/detail.js
+++ public/javascripts/controllers/detail.js
@@ -1,4 +1,70 @@
 angular.module('myApp')
-.controller('detailCtrl', function($scope,$mdBottomSheet) {
-
+.controller('detailCtrl', function($scope, $mdBottomSheet) {
+	$scope.messages = [{
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, {
+		face: '/imgs/list/list.jpg',
+		what: 'Brunch this weekend?',
+		who: 'Min Li Chan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+	}, ];
 })
